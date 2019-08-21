98d5e3d32676d76ed68c4d0d45e525b334a0c5a2
gbleigh5 <slystan7@gmail.com>
2019-08-21 14:54:07 +0300

    Update mychanges.sh
    I am yet to sort out assigning the directory into a variable. When I do I will let you know

--- mychanges.sh
+++ mychanges.sh
@@ -4,36 +4,20 @@ mkdir ~/terminal/www
 cd ~/terminal/www/
 echo "www folder is created"
 echo "As soon as you are reading this, 6 files and 3 directories are being created"
-touch .hidden.env
-touch index.html
-touch services.html
-touch services.old.html
-touch about.html
-touch about.old.html
-touch robot.txt
-touch debug.log
-touch error.log
+touch .hidden.env index.html services.html services.old.html about.html about.old.html robot.txt debug.log error.log
 mkdir ~/terminal/www/assets
 mkdir ~/terminal/www/dist
 mkdir ~/terminal/www/trash
-ls -a
+ls -lat
 cd ~/terminal/www/assets
 echo "Three files are being created in the assets directory"
-touch logo.png
-touch banner.png
-touch styles.css
+touch logo.png banner.png styles.css
 ls
 cd ..
-rm error.log
-rm debug.log
-mv services.old.html ~/terminal/www/trash
-mv about.old.html ~/terminal/www/trash
+rm *.log
+mv *.old.html ~/terminal/www/trash
 echo ".old files have been moved to the trash directory."
-cp index.html ~/terminal/www/dist
-cp services.html ~/terminal/www/dist
-cp about.html ~/terminal/www/dist
-cp .hidden.env ~/terminal/www/dist
-cp robot.txt ~/terminal/www/dist
+cp *.html robot.txt .hidden.env ~/terminal/www/dist
 cd ~/terminal/www/dist
 ls
 cd ..
