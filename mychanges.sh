a995d57016bffa47649c2c26abacb55ca57a1958
gbleigh5 <slystan7@gmail.com>
2019-08-26 02:09:03 +0300

    Update mychanges.sh
    Trust me it works (It's the one I sent to you on WhatsApp). 
    The code just allows you to input any directory name of your choice in the terminal.
     If you don't input one, it prompts you to

--- mychanges.sh
+++ mychanges.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
-echo "Welcome!!"
-mkdir ~/terminal/www
-cd ~/terminal/www/
-echo "www folder is created"
-echo "As soon as you are reading this, 6 files and 3 directories are being created"
+if (( $# < 1 )); then
+    echo "Usage: $(basename -- "$0") path" >&2
+    exit 2
+fi
+
+if ! mkdir -p -- "$1"; then
+    echo "Cannot create directory $1" >&2
+    exit 1
+fi
+
+if ! cd -- "$1"; then
+    echo "Cannot cd to directory $1" >&2
+    exit 1
+fi
+
+# -----------
+
+echo 'As soon as you are reading this, 6 files and 3 directories are being created'
 touch .hidden.env index.html services.html services.old.html about.html about.old.html robot.txt debug.log error.log
-mkdir ~/terminal/www/assets
-mkdir ~/terminal/www/dist
-mkdir ~/terminal/www/trash
-ls -lat
-cd ~/terminal/www/assets
-echo "Three files are being created in the assets directory"
-touch logo.png banner.png styles.css
-ls
-cd ..
+
+mkdir assets dist trash
+ls -ln
+
+echo 'Three files are being created in the assets directory'
+touch assets/logo.png assets/banner.png assets/styles.css
+ls assets
+
 rm *.log
-mv *.old.html ~/terminal/www/trash
+mv *.old.html trash/
 echo ".old files have been moved to the trash directory."
-cp *.html robot.txt .hidden.env ~/terminal/www/dist
-cd ~/terminal/www/dist
-ls
-cd ..
+
+cp *.html robot.txt .hidden.env dist/
+ls dist
+
 rm -r trash
-mkdir ~/terminal/www/public_html
-cp -r dist ~/terminal/www/public_html
+mkdir public_html
+cp -r dist public_html
+
